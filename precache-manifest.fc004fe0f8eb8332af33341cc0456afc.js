e291b62a25cfefb404216d5dbdea3b9f0c019607
Filip Handzel <f.handzel@gmail.com>
2022-06-09 17:19:43 +0200

    Updates

---
+++ precache-manifest.fc004fe0f8eb8332af33341cc0456afc.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "12ae6afec3e2cacd3079",
+    "url": "/static/js/main.ae7b8835.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "837b3f2c6d1df1a86b47",
+    "url": "/static/js/2.9e385457.chunk.js"
+  },
+  {
+    "revision": "93d4f3ba834e7afdf3f8dcb93f2a82f8",
+    "url": "/static/media/logo.93d4f3ba.svg"
+  },
+  {
+    "revision": "af737b5505b4cdb4a433cc3ac1f234af",
+    "url": "/static/media/bulb.af737b55.svg"
+  },
+  {
+    "revision": "84b70f8a7155a0edb0576db0efc9ace2",
+    "url": "/static/media/pen.84b70f8a.svg"
+  },
+  {
+    "revision": "0969c92b498df082b2f18fc455b2fa13",
+    "url": "/static/media/logout.0969c92b.svg"
+  },
+  {
+    "revision": "a14461b86078261a696a425d13ffe612",
+    "url": "/static/media/magnifier.a14461b8.svg"
+  },
+  {
+    "revision": "e43ce6c85c8383cb4442972a422cf804",
+    "url": "/static/media/plus.e43ce6c8.svg"
+  },
+  {
+    "revision": "6fc1d8fe5a3f17e73f1ecd0d757475d1",
+    "url": "/static/media/link.6fc1d8fe.svg"
+  },
+  {
+    "revision": "dfefd299c7a3f127ae99f06a53c994e1",
+    "url": "/index.html"
+  }
+];
